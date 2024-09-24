993c3b58bd32226f69bf20c3a1b85b78f773f9e7
Erno Lokkila <eolokk@utu.fi>
2024-09-24 23:09:01 +0300

    Ensimmäinen täysi sivun proto (?).

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+var curZ = 0
+var currTouch = null;
+
 function onClose(target){ target.remove(); }
 	function on(target){ target.remove(); }
 
@@ -8,9 +11,9 @@ function dragElement(elmnt) {
     document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
   } else {
     // otherwise, move the DIV from anywhere inside the DIV:
-    elmnt.onmousedown = dragMouseDown;
+    elmnt.onmousedown = dragMouseDown;	
   }
-
+		
   function dragMouseDown(e) {
     e = e || window.event;
     e.preventDefault();
@@ -19,10 +22,13 @@ function dragElement(elmnt) {
     pos4 = e.clientY;
     document.onmouseup = closeDragElement;
     // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
+    document.onmousemove = elementDrag;	
+	elmnt.style.zIndex = curZ;
+	curZ += 1
   }
 
   function elementDrag(e) {
+	  console.log("draggin");
     e = e || window.event;
     e.preventDefault();
     // calculate the new cursor position:
@@ -33,11 +39,40 @@ function dragElement(elmnt) {
     // set the element's new position:
     elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
     elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	elmnt.style.zIndex = curZ;
   }
 
   function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
-    document.onmousemove = null;
+    document.onmousemove = null;	
   }
+}
+function registerTouch(elmnt){
+	var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+	elmnt.addEventListener("touchstart", handleTouchStart);
+	elmnt.addEventListener("touchend", handleTouchEnd);	
+	elmnt.addEventListener("touchcancel", handleTouchEnd);
+	elmnt.addEventListener("touchmove", handleTouchMove);
+		
+	function handleTouchEnd(e){ currTouch = null; }	
+	function handleTouchStart(e){ 
+		currTouch = e; 
+	    pos3 = e.touches[0].screenX;
+		pos4 = e.touches[0].screenY;
+	}	
+	function handleTouchMove(e) {
+		if (currTouch == null) return;
+		e.preventDefault();
+		var touchX = e.touches[0].screenX;
+		var touchY = e.touches[0].screenY;
+		pos1 = pos3 - touchX;
+		pos2 = pos4 - touchY;
+		pos3 = touchX;
+		pos4 = touchY;
+		// set the element's new position:
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+		elmnt.style.zIndex = curZ;
+	}
 }
